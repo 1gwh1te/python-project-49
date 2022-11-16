0a22ef8f26e16b764a5690d781714b0c527e9f87
gary white <ibelousovv@gmail.com>
2022-11-16 20:01:09 +0300

    update brain_even flake8

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,10 @@
 import prompt
 import random
 
+
 print("Welcome to the Brain Games!")
 name = prompt.string('May I have your name? ')
+
 
 def is_even(num):
     if num % 2 == 0:
@@ -10,27 +12,30 @@ def is_even(num):
     else:
         return False
 
+
 def question(num):
     response = ''
     result = 0
     num = random.randint(0, 100)
-    answer = prompt.string(f'Question: {num}\nYour answer: {response}')
+    answer = prompt.string(f'Question: {num} \nYour answer: {response}')
     if answer != 'yes' and answer != 'no':
-        print(f"\{answer}\" is wrong answer ;(\nLet's try again, {name}!")
+        print(f"\"{answer}\" is wrong answer ;( \nLet's try again, {name}!")
         result = 3
-    if is_even(num) == True and answer == 'yes':
+    if is_even(num) is True and answer == 'yes':
         print('Correct!')
         result = 1
-    if is_even(num) == False and answer == 'no':
+    if is_even(num) is False and answer == 'no':
         print('Correct!')
         result = 1
-    if is_even(num) == True and answer == 'no':
+    if is_even(num) is True and answer == 'no':
         print(f"\"no\" is wrong answer ;(. Correct answer was \'yes\'\nLet's try again, {name}!")
         result = 3
-    if is_even(num) == False and answer == 'yes':
+    if is_even(num) is False and answer == 'yes':
         print(f"\"yes\" is wrong answer ;(. Correct answer was \'no\'\nLet's try again, {name}!")
         result = 4
     return result
+
+
 def main():
     index = 1
     print(f'Hello, {name}!')
@@ -39,9 +44,11 @@ def main():
         result = question(index)
         if result == 3 or result == 4:
             break
-        if index == 3 and result == 1 :
+        if index == 3 and result == 1:
             print(f'Congratulations, {name}')
             break
         index += 1
+
+
 if __name__ == '__main__':
     main()
